be14a012cc40377fb5feb59ca5c68bd95e4228ac
Frank <ananwill@outlook.com>
2022-08-21 16:04:13 +0800

    test code push

---
+++ flask_cache.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/21 15:19
+# @Author  : Frank
+# @File    : flask_cache.py
+# @Software: PyCharm
+
+
+from flask.ext.cache import Cache
+from flask import Flask
+
+
